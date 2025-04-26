ac5eb7b862c90ab6d5463061a934ab7dd5154d22
Johannes Maier <johannes.maier@mailbox.org>
2025-04-26 23:08:49 +0200

    read raw record metadata
    These seem to be descriptions of where to find uncompressed bytes in the
    database that then *really* constitute some *thing*, as well as info
    about its size and type.
    
    Next we need to decompress that data, and probably filter for things
    that "are items".

--- database/database.go
+++ database/database.go
@@ -20,46 +20,74 @@ func newReader(file string) (error, *reader) {
 	return nil, &reader{&bytes, 0}
 }
 
-func (d *reader) getBytes(count uint) []byte {
-	bytes := (*d.data)[d.cursor : d.cursor+count]
-	d.cursor += count
+func (r *reader) getBytes(count uint) []byte {
+	bytes := (*r.data)[r.cursor : r.cursor+count]
+	r.cursor += count
 	return bytes
 }
 
-func (d *reader) readUint16() uint16 {
-	bytes := d.getBytes(2)
+func (r *reader) readUint16() uint16 {
+	bytes := r.getBytes(2)
 	return binary.LittleEndian.Uint16(bytes)
 }
 
-func (d *reader) readUint32() uint32 {
-	bytes := d.getBytes(4)
+func (r *reader) readUint32() uint32 {
+	bytes := r.getBytes(4)
 	return binary.LittleEndian.Uint32(bytes)
 }
 
-func (d *reader) readString() string {
-	length := uint(d.readUint32())
-	remaining := uint(len(*d.data)) - d.cursor
+func (r *reader) readString() string {
+	length := uint(r.readUint32())
+	remaining := uint(len(*r.data)) - r.cursor
 	if length > remaining || length > 10*1024 {
-		fmt.Printf("  encountered strange length: %d, remaining: %d\n", length, remaining)
+		// fmt.Printf("  encountered strange length: %d, remaining: %d\n", length, remaining)
 		return ""
 	}
-	return string(d.getBytes(length))
+	return string(r.getBytes(length))
 }
 
-func (d *reader) getStringTable(start uint, byteCount uint) []string {
+func (r *reader) getStringTable(start uint, byteCount uint) []string {
 	end := start + byteCount
 	var strings []string
-	d.cursor = start
-	for d.cursor < end {
-		n := d.readUint32()
+	r.cursor = start
+	for r.cursor < end {
+		n := r.readUint32()
 		for range n {
-			strings = append(strings, d.readString())
+			strings = append(strings, r.readString())
 		}
 	}
 	return strings
 }
 
-func GetItemDeclarations(file string) (error, []string) {
+type itemDeclarationMeta struct {
+	stringIndex      uint32
+	name             string
+	pos              uint32
+	compressedSize   uint32
+	uncompressedSize uint32
+}
+
+func (r *reader) readItemDeclarationMeta() itemDeclarationMeta {
+	stringIndex := r.readUint32()
+	name := r.readString()
+	offset := r.readUint32()
+	compressedSize := r.readUint32()
+	uncompressedSize := r.readUint32()
+	r.cursor += 8
+	return itemDeclarationMeta{stringIndex, name, offset, compressedSize, uncompressedSize}
+}
+
+func (r *reader) readItemDeclarations(start uint, count uint) []itemDeclarationMeta {
+	r.cursor = start
+	fmt.Printf("trying to read %d meta records\n", count)
+	var metas []itemDeclarationMeta
+	for range count {
+		metas = append(metas, r.readItemDeclarationMeta())
+	}
+	return metas
+}
+
+func GetItemDeclarations(file string) (error, []itemDeclarationMeta) {
 	err, reader := newReader(file)
 	if err != nil {
 		return err, nil
@@ -75,10 +103,19 @@ func GetItemDeclarations(file string) (error, []string) {
 		return fmt.Errorf("unsupported version: %d", version), nil
 	}
 
+	recordStart := reader.readUint32()
 	_ = reader.readUint32()
-	_ = reader.readUint32()
-	_ = reader.readUint32()
-	stringTableStart := reader.readUint32()
-	stringTableSize := reader.readUint32()
-	return nil, reader.getStringTable(uint(stringTableStart), uint(stringTableSize))
+	recordCount := reader.readUint32()
+	stringStart := reader.readUint32()
+	stringByteCount := reader.readUint32()
+
+	strings := reader.getStringTable(uint(stringStart), uint(stringByteCount))
+	fmt.Printf("found %d strings in %s\n", len(strings), file)
+
+	// FIXME: this is inefficient/naive as a first pass. use more efficient
+	// arguments (iterator, pointer), but measure beforehand to learn things
+	// about go!
+	metas := reader.readItemDeclarations(uint(recordStart), uint(recordCount))
+
+	return nil, metas
 }
