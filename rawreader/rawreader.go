64622022cf564e6ca123f1609eeec209b7093782
Johannes Maier <johannes.maier@mailbox.org>
2025-05-12 07:01:22 +0200

    rawreader: add docstrings

--- rawreader/rawreader.go
+++ rawreader/rawreader.go
@@ -7,15 +7,18 @@ import (
 	"os"
 )
 
+// A read-only view on some binary data.
 type T struct {
 	data   []byte
 	cursor uint32
 }
 
+// Create a new reader.
 func New(data []byte) *T {
 	return &T{data, 0}
 }
 
+// Create a new reader from a file by reading in all its data at once.
 func FromFile(file string) (*T, error) {
 	data, err := os.ReadFile(file)
 	if err != nil {
@@ -25,44 +28,53 @@ func FromFile(file string) (*T, error) {
 	return New(data), nil
 }
 
+// Jump to the specified `cursor` position.
 func (reader *T) Seek(cursor uint32) {
 	reader.cursor = cursor
 }
 
+// Move `offset` bytes relative to the current cursor position.
 func (reader *T) Advance(offset uint32) {
 	reader.cursor += offset
 }
 
+// From the position `start`, get a slice of `count` bytes.
 func (reader *T) BytesFrom(start uint32, count uint32) []byte {
 	bytes := reader.data[start : start+count]
 	reader.cursor += count
 	return bytes
 }
 
+// Get a slice of `count` bytes from the current cursor position.
 func (reader *T) Bytes(count uint32) []byte {
 	return reader.BytesFrom(reader.cursor, count)
 }
 
+// Read a `uint16` at the current position.
 func (reader *T) Uint16() uint16 {
 	bytes := reader.Bytes(2)
 	return binary.LittleEndian.Uint16(bytes)
 }
 
+// Read a `uint32` at the current position.
 func (r *T) Uint32() uint32 {
 	bytes := r.Bytes(4)
 	return binary.LittleEndian.Uint32(bytes)
 }
 
+// Read a `uint64` at the current position.
 func (r *T) Uint64() uint64 {
 	b := r.Bytes(8)
 	return binary.LittleEndian.Uint64(b)
 }
 
+// Read a `float32` at the current position.
 func (r *T) Float32() float32 {
 	n := r.Uint32()
 	return math.Float32frombits(n)
 }
 
+// Read a `string` at the current position.
 func (r *T) String() string {
 	length := r.Uint32()
 	remaining := uint32(len(r.data)) - r.cursor
@@ -72,6 +84,9 @@ func (r *T) String() string {
 	return string(r.Bytes(length))
 }
 
+// Read a C string at the current position; that is, read until encountering a
+// terminal '\0'.
+//
 // FIXME: which one of the string variants is really necessary?
 func (r *T) CString() string {
 	start := r.cursor
