4e151177f600a578f9d20f5cbe56568db3e00097
Johannes Maier <johannes.maier@mailbox.org>
2025-05-18 19:50:36 +0200

    frontend: add react project

---
+++ frontend/prettier.config.js
@@ -0,0 +1,10 @@
+const config = {
+    useTabs: false,
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 100,
+    plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+};
+
+export default config;
